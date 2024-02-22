76f23330e3cd74ec0d09c2e4e1f3ab198b52aad1
mayurdamre111 <mayurdamre111@gmail.com>
2024-02-22 11:00:44 +0530

    This is my first commit of advance js and react js code

---
+++ Day 2/first.js
@@ -0,0 +1,56 @@
+// 22 feb 2024
+/*
+
+console.log("Hello we are in TS");
+
+var a; // declaration
+a = 10; // initialization
+a= 15;
+a= 66; // can be declared & reinitialized
+
+console.log(a);
+
+let x= 20; // can be reinitialized only
+x=55;
+console.log(x);
+
+const p =30; // can't be declared & reinitialized
+
+console.log(p);
+
+*/
+
+/*
+//Hoisting
+
+console.log(a);
+var a=10;
+
+*/
+/*
+// javascript types --> (primitive and references{},[],())
+
+//Primitive
+var a = 20;
+var b = a;
+b= b+2;
+console.log(b);
+console.log(a);
+
+//References
+var arr = [1,2,3,4,5];
+//console.log(arr);
+var arr2 = arr;
+arr2 [3] = 100;
+console.log(arr2);
+console.log(arr);
+*/
+
+var brr =[1,2,3,4,5];
+brr[2]="Hello";
+
+brr[-1]="hehe"; // it store in object (key value pair)
+
+
+console.log(brr);
+
